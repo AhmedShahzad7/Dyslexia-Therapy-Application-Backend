6c79a699ab526f0e515ec270a9fa78e799a956a4
iMuhammad-pk <muhammadmasood1557@gmail.com>
2026-01-04 21:22:48 +0500

    updated model

--- app.py
+++ app.py
@@ -7,12 +7,19 @@ from tensorflow.keras.preprocessing import image
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from google import genai
 
-
+img_height=64
+img_width=64
 
 app = Flask(__name__)
 
 model = tf.keras.models.load_model("letter_model.h5")
+f=open('details.txt')
+text=f.read()
+f.close()
+print(text)
+client = genai.Client(api_key="AIzaSyBT0alA9j4koF6ihAplZLLrCxryYfUTo4A")
 
 
 try:
@@ -27,7 +34,10 @@ def predict():
     print("RECEIVED REQUEST!!")
     file = request.files["file"]
     img = Image.open(file.stream).convert("RGB")  
-    img = img.resize((64, 64))
+    img = img.resize((img_height
+
+, img_width
+))
     img_array = image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
     plt.imshow(img)
@@ -41,9 +51,17 @@ def predict():
 
     confidence = np.max(predictions)
     print(f"Predicted Letter: {predicted_class} with confidence {confidence:.2f}")
-
+    prompt = (
+        "You will be our handwriting identifier you will detect which letter is shown in the image and it should only be 1 letter(only english letters if you see something else put -) answer from you for example if the image shows A then you will say A_caps if its a then you will say a"
+    )
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=[prompt, img]
+    )
+    gemini_analysis = response.text
+    print('GEMINI RESPONSE',gemini_analysis)
 
     return {"prediction": predicted_class}
 
 if __name__ == "__main__":
-    app.run(host='192.168.1.2', port=5000)
+    app.run(host='192.168.1.3', port=5000)
